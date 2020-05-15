14ce694b3153cba573ad359138305f4673a5c2d5
Fadil Muh <fadilmuh2002@gmail.com>
2020-05-15 21:32:14 +0700

    coomit pertama, semoga project sukses

---
+++ src/main/java/com/empat/kelasku/Main.java
@@ -0,0 +1,10 @@
+package com.empat.kelasku;
+
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Fadil");
+    }
+}
